c4361c96603560fb7e21d3c1dbe45a67b4298b3d
liaoxuewu <liaoxuewu1993@163.com>
2022-08-01 09:51:08 +0800

    page of detail add variable and methods

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,10 +1,59 @@
 <template>
-  <div></div>
+  <div>
+    <van-nav-bar :title="titleName" left-arrow @click-left="goBack" fixed placeholder z-index="99999"> </van-nav-bar>
+    <van-pull-refresh v-model="isShowLoading" @refresh="onRefresh" v-if="dataList.length">
+    </van-pull-refresh>
+    <g-empty v-else></g-empty>
+  </div>
 </template>
 
 <script>
+import config from "../../../public/config";
+
 export default {
-  name: 'Detail'
+  name: 'Detail',
+  data() {
+    return {
+      titleName: '详情',
+      isShowLoading: false,
+      dataList: []
+    }
+  },
+  mounted() {
+    this.appId = config.appId
+    this.init()
+  },
+  methods: {
+    /**
+     * 初始化
+     */
+    init() {
+      this.isShowLoading = true
+      try {
+        this.reset()
+        this.getDataList()
+      } catch (e) {
+        console.log(e)
+      } finally {
+        this.isShowLoading = false
+      }
+    },
+    /**
+     * 刷新获取数据
+     */
+    onRefresh() {
+      setTimeout(() => {
+        this.init()
+      }, 1000)
+    },
+    /**
+     * 返回
+     */
+    goBack() {
+      this.$router.back(-1)
+      // this.$router.push('/')
+    }
+  }
 }
 </script>
 
