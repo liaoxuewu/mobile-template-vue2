138dbe15a72cc6915228f22afc6f93b3d5b9ecb5
liaoxuewu <liaoxuewu1993@163.com>
2022-08-16 16:38:50 +0800

    add vant.js

--- src/main.js
+++ src/main.js
@@ -10,50 +10,15 @@ import 'vant/lib/index.css' //必须引入组件样式
 import 'lib-flexible/flexible.js' //字体适配
 import './assets/main.css'
 import './plugins' // 引入插件
-
-import { Icon, Cell, CellGroup, Loading, Button, Toast } from 'vant'
-import {
-  Search,
-  Tabbar,
-  TabbarItem,
-  NavBar,
-  ActionSheet,
-  PullRefresh,
-  Tab,
-  Tabs,
-  Collapse,
-  CollapseItem,
-  Popup
-} from 'vant'
 import preview from 'vue-photo-preview'
 import 'vue-photo-preview/dist/skin.css'
 import './assets/font/font-awesome-4.7.0/css/font-awesome.css'
 import VConsole from 'vconsole/dist/vconsole.min.js'
-let vConsole = new VConsole()
 let options = {
   fullscreenEl: false
 }
 Vue.use(preview, options)
 Vue.use(preview)
-
-Vue.use(Collapse)
-Vue.use(CollapseItem)
-Vue.use(NavBar)
-Vue.use(PullRefresh)
-Vue.use(ActionSheet)
-Vue.use(Search)
-Vue.use(Tab)
-Vue.use(Tabs)
-Vue.use(Tabbar)
-Vue.use(TabbarItem)
-Vue.use(Icon)
-Vue.use(Cell)
-Vue.use(CellGroup)
-//Vue.use(loading);
-Vue.use(Button)
-Vue.use(Popup)
-Vue.use(Loading)
-Vue.use(Toast)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/plugins/modules/vant.js
@@ -0,0 +1,68 @@
+/**
+ * @Description:
+ * @Author liaoxuewu
+ * @create 2022/07/20 18:05
+ */
+import Vue from 'vue'
+import {
+  ActionSheet,
+  Search,
+  PullRefresh,
+  Toast,
+  Loading,
+  Popup,
+  NavBar,
+  Tabbar,
+  TabbarItem,
+  Icon,
+  Empty,
+  Collapse,
+  CollapseItem,
+  Tab,
+  Tabs,
+  Sticky,
+  Col,
+  Row,
+  Cell,
+  CellGroup,
+  ImagePreview,
+  Sidebar,
+  SidebarItem,
+  Swipe,
+  SwipeItem,
+  Popover,
+  Picker,
+  Button,
+  Pagination
+} from 'vant'
+Vue.use(Tabbar)
+Vue.use(TabbarItem)
+Vue.use(ActionSheet)
+Vue.use(Search)
+Vue.use(PullRefresh)
+Vue.use(Toast)
+Vue.use(Loading)
+Vue.use(Popup)
+Vue.use(NavBar)
+Vue.use(Tabbar)
+Vue.use(TabbarItem)
+Vue.use(Icon)
+Vue.use(Empty)
+Vue.use(Collapse)
+Vue.use(CollapseItem)
+Vue.use(Tab)
+Vue.use(Tabs)
+Vue.use(Sticky)
+Vue.use(Col)
+Vue.use(Row)
+Vue.use(ImagePreview)
+Vue.use(Cell)
+Vue.use(CellGroup)
+Vue.use(Sidebar)
+Vue.use(SidebarItem)
+Vue.use(Swipe)
+Vue.use(SwipeItem)
+Vue.use(Popover)
+Vue.use(Picker)
+Vue.use(Button)
+Vue.use(Pagination)
